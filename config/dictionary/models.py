a82e353fa1bac2977d8543a955ab2df024dcc73a
daniil <daniilcehmestrenko@icloud.com>
2022-12-07 19:13:15 +0300

    add models

---
+++ config/dictionary/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+
+from users.models import User
+
+
+class MyAllergy(models.Model):
+    name = models.CharField(
+            max_length=100,
+            verbose_name='Название'
+        )
+    carriers = models.ManyToManyField(
+            'Carrier',
+            verbose_name='Носители'
+        )
+    user = models.OneToOneField(
+            User,
+            on_delete=models.CASCADE,
+            verbose_name='Пользователь'
+        )
+
+
+class Category(models.Model):
+    name = models.CharField(
+            max_length=100,
+            verbose_name='Название'
+        )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Carrier(models.Model):
+    name = models.CharField(
+            max_length=100,
+            verbose_name='Название'
+        )
+    description = models.CharField(
+            max_length=1000,
+            verbose_name='Описание'
+        )
+    category = models.ForeignKey(
+            'Category',
+            on_delete=models.DO_NOTHING,
+            verbose_name='Категория',
+            related_name='carriers'
+        )
+    allergen = models.ForeignKey(
+            'Allergen',
+            on_delete=models.DO_NOTHING,
+            verbose_name='Аллерген',
+            related_name='allergens'
+        )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Носитель'
+        verbose_name_plural='Носители'
+
+
+class Allergen(models.Model):
+    name = models.CharField(
+            max_length=100,
+            verbose_name='Аллерген'
+        )
+    description = models.CharField(
+            max_length=1000,
+            verbose_name='Описание'
+        )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Аллерген'
+        verbose_name_plural = 'Аллергены'
